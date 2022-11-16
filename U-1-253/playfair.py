2aaa25bc2b62142bd109abfa134dffe7b0dba79a
Noel Pascal Schwabenland <noel@schwabenland.ch>
2022-11-16 21:41:22 +0100

    added solution for U-1-253

---
+++ U-1-253/playfair.py
@@ -0,0 +1,63 @@
+import sys
+from utils import *
+
+
+def _1(path_to_messgae):
+    key = remove_duplicates(replace_j_with_i(remove_non_letters([x.upper() for x in sys.argv[1]])))
+    text = replace_j_with_i(read_text_from_file(path_to_messgae))
+    return key, text
+
+def _2(key):
+    rest = [x for x in [chr(x) for x in range(ord("A"), ord("Z")+1)] if (x not in key and x != "J")]
+    matrix = [(key+rest)[i*5:i*5+5] for i in range(5)]
+    return matrix
+
+def _3(text):
+    # Add X between two equal letters
+    for i in range(len(text)-1):
+        if text[i] == text[i+1]:
+            text.insert(i+1, "X")
+    
+    # Add X at the end if the text has an odd length
+    if len(text) % 2 == 1:
+        text.append("X")
+
+    bigraph_liste = [(text[i]+text[i+1]) for i in range(0, len(text), 2)]
+
+    return bigraph_liste
+
+def _4(matrix, bigraph_liste):
+    return encode_playfair(matrix, bigraph_liste)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python playfair.py <key> <?text-file-path>")
+        sys.exit(1)
+
+    path_to_messgae = sys.argv[2] if len(sys.argv) == 3 else "lorem-ipsum.txt"
+
+    # 1.
+    key, text = _1(path_to_messgae)
+    print(f"key={key}")
+    print(f"text={text}")
+
+    # 2.
+    matrix = _2(key)
+    print(f"matrix={matrix}")
+
+    # 3.
+    bigraph_liste = _3(text)
+    print(f"bigraph_liste={bigraph_liste}")
+
+    # 4.
+    chiffre = _4(matrix, bigraph_liste)
+    print(f"chiffre={chiffre}")
+
+    # 5.
+    message = decode_playfair(matrix, chiffre)
+    print(f"decoded_bigraph_liste={message}")
+    # Test if decoding worked
+    assert message == bigraph_liste
+    # Revert bigraphs to text
+    message = "".join(message).replace("X", "")
+    print(f"decoded_text={message}")
---
+++ U-1-253/utils.py
@@ -0,0 +1,54 @@
+
+def remove_non_letters(text):
+    return [x for x in text if x in [chr(x) for x in range(ord("A"), ord("Z")+1)]]
+
+def read_text_from_file(path_to_messgae):
+    text = ""
+    # Read text from test-text.txt
+    with open(path_to_messgae, "r", encoding="utf-8") as file:
+        text = remove_non_letters([x.upper() for x in file.read()])
+    return text
+
+def replace_j_with_i(text):
+    return [x if x != "J" else "I" for x in text]
+
+def remove_duplicates(text):
+    return list(dict.fromkeys(text))
+
+def get_coordinates(matrix, letter):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == letter:
+                return i, j
+
+def playfair_algorithus(matrix, bigraph_liste, reverse=False):
+    chiffre = ["" for _ in bigraph_liste]
+    for bigraph_index, bigraph in enumerate(bigraph_liste):
+        x1, y1 = get_coordinates(matrix, bigraph[0])
+        x2, y2 = get_coordinates(matrix, bigraph[1])
+
+        # (a) Beide Buchstaben liegen in derselben Zeile
+        if y1 == y2:
+            # Jeder Buchstabe wird jeweils durch den rechten Nachbarn ersetzt (periodische Ränder)
+            x1 = (x1 + (- 1 if reverse else 1)) % 5
+            x2 = (x2 + (- 1 if reverse else 1)) % 5
+
+        # (b) Beide Buchstaben liegen in derselben Spalte
+        elif x1 == x2:
+            # Jeder Buchstabe wird jeweils durch den unteren Nachbarn ersetzt (periodische Ränder)
+            y1 = (y1 + (- 1 if reverse else 1)) % 5
+            y2 = (y2 + (- 1 if reverse else 1)) % 5
+
+        # (c) Beide Buchstaben bilden zwei Ecken eines gedachten Quadrats innerhalb der Matrix
+        else:
+            # Jeder Buchstabe wird jeweils durch einen derbeiden andren "Eckbuchstaben" ersetzt
+            y1, y2 = y2, y1
+        chiffre[bigraph_index] = matrix[x1][y1] + matrix[x2][y2]
+
+    return chiffre
+
+def encode_playfair(matrix, bigraph_liste):
+    return playfair_algorithus(matrix, bigraph_liste)
+
+def decode_playfair(matrix, chiffre):
+    return playfair_algorithus(matrix, chiffre, reverse=True)
